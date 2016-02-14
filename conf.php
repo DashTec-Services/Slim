2982f9502d3813cd0d20fd049ecbb59f417ce663
DashTec-Services <info@dashtec.de>
2016-02-14 13:50:48 +0100

    up

--- conf.php
+++ conf.php
@@ -1,22 +1,50 @@
 <?php
+/**
+ * Created by David Schomburg (DashTec - Services)
+ *      www.dashtec.de
+ *
+ *  S:P (StreamersPanel)
+ *  Support: http://board.streamerspanel.de
+ *
+ *  v 4.0.0
+ *
+ *  Kundennummer:   @KDNUM@
+ *  Lizenznummer:   @RECHNR@
+ *  Lizenz: http://login.streamerspanel.de/user/terms
+ */
+
+# Prüfung das install_DIR gelöscht wurde
+if(is_dir("./install") ){
+    // TODO Realease activate
+    #die(_('Bitte den "install" Ordner löschen'));
+}
 
 
-$app = new Slim\App([
+$app = new \Slim\Slim([
+    'sp.version' => '4.0.0',
+    'sp.version_clear' => '400',
+    'db.user' => 'spdevuser',              # @DBUSER@ sapdevuser
+    'db.password' => '', 		    # @DBPASS@ jaro2812
+    'db.name' => 'spdev',		            # @DBNAME@
+    'demo_mod' => false,                     # @DEMOMOD@
+    'view' => new \SP\Views\MyUltimateView(),
+    'templates.path' => __DIR__ . '/views',
+    'db.host' => 'localhost',
+    'db.port' => 3306,
+    'db.encoding' => 'utf8',
+    'php.timezone' => 'Europe/Berlin',
     'php.error-reporting' => E_ALL | E_STRICT,
     'middleware.authentication' => [
         'filter_mode' => \SP\Middleware\AbstractFilterableMiddleware::EXCLUSION,
-        'route_names' => ['login', 'doLogin', 'doLogout', 'logout'],
+        'route_names' => ['login', 'doLogin', 'doLogout'],
     ],
     'middleware.authorization' => [
         'filter_mode' => \SP\Middleware\AbstractFilterableMiddleware::INCLUSION,
-        'route_names' => ['adm_admin', 'admin', 'reseller', 'user','ekuser','restricted','sms'],
+        'route_names' => ['restricted'], ['userrestricted'],
         'route_group_mappings' => [
-            'adm_admin' => ['adm_admin'],
-            'admin' => ['admin'],
-            'reseller' => ['reseller'],
-            'user' => ['user'],
-            'ekuser' => ['ekuser'],
-            'restricted' => ['adm_admin','admin', 'reseller' ,'user', 'ekuser'],
+            'restricted'    => ['adm'],
+            'userrestricted'    => ['user' , 'dj'],
         ],
     ],
 ]);
+
--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
-$lifetime=90000;
+/**
+ * Created by David Schomburg (DashTec - Services)
+ *      www.dashtec.de
+ *
+ *  S:P (StreamersPanel)
+ *  Support: http://board.streamerspanel.de
+ *
+ *  v 4.5.0
+ *
+ *  Kundennummer:   @KDNUM@
+ *  Lizenznummer:   @RECHNR@
+ *  Lizenz: http://login.streamerspanel.de/user/terms
+ */
 session_start();
-setcookie(session_name(),session_id(),time()+$lifetime);
-# MicroTime for Devel.
-$beginn = microtime(true);
+#$version = $_SERVER['SERVER_SOFTWARE'];
+#echo "$version\n";
+#echo '<br>Die aktuelle PHP Version ist ' . phpversion();
+
+ini_set('display_errors', 1);
+
+if(!is_file("conf.php") ){
+    // TODO Realease activate
+    die(_('Bitte das Panel Installieren'));
+}
 
 require_once __DIR__ . '/core/SplClassLoader.php';
 require_once __DIR__ . '/core/DB.php';
@@ -11,61 +30,62 @@ include_once __DIR__ . '/core/request/Requests.php';
 
 (new SplClassLoader('Slim', __DIR__ . '/core'))->register();
 (new SplClassLoader('SP', __DIR__ . '/core'))->register();
-(new SplClassLoader('core', __DIR__))->register();
+(new SplClassLoader('core', __DIR__ ))->register();
 
-#require_once __DIR__ . '/conf.php';
+require_once __DIR__ . '/conf.php';
 
 $app->add(new \SP\Middleware\AuthorizationMiddleware());
 $app->add(new \SP\Middleware\AuthenticationMiddleware());
 
-
-
-require_once __DIR__ . '/GlobeVar.php';
-
-# Basic - Routes
-#require_once __DIR__ . '/config/database.php';
-#require_once __DIR__ . '/routes/authentication.php';
-
-
-# Routes by User
-require_once __DIR__ . '/auto_route_loader.php';
-
-# DOMAIN - ERKENNUNG
-$host = explode('.', $_SERVER['HTTP_HOST']);
-$subdomain = $host[0];
-
+# Active Routes
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/routes/authentication.php';
+require_once __DIR__ . '/routes/support.php';
+require_once __DIR__ . '/routes/sp.php';
+require_once __DIR__ . '/routes/news.php';
+require_once __DIR__ . '/routes/station.php';
+require_once __DIR__ . '/routes/station_icecast.php';
+require_once __DIR__ . '/routes/filemanager.php';
+require_once __DIR__ . '/routes/server.php';
+require_once __DIR__ . '/routes/benutzerverwaltung.php';
+require_once __DIR__ . '/routes/dj.php';
+require_once __DIR__ . '/routes/streamAddSel.php';
+require_once __DIR__ . '/routes/djuserfunction.php';
+require_once __DIR__ . '/routes/stream_edit_control.php';
+require_once __DIR__ . '/routes/playlist.php';
 
 # GETTEXT Cofig
-if (empty($_SESSION['local'])) {
-#   $local = DB::queryFirstRow("SELECT default_local FROM config WHERE id=%s", '1');
-  #  $_SESSION['local'] = $local['default_local'];
+if(empty($_SESSION['local'])){
+    $local = DB::queryFirstRow("SELECT default_local FROM config WHERE id=%s",'1');
+    $_SESSION['local'] = $local['default_local'];
 }
 $local = $_SESSION['local'];  # de_DE , en_US, es_MX
-putenv("LC_ALL=" . $local . ".utf8");
-setlocale(LC_ALL, $local . ".utf8");
-bindtextdomain($local, __DIR__ . '/locale');
+putenv("LC_ALL=".$local.".utf8");
+setlocale(LC_ALL, $local.".utf8");
+bindtextdomain($local, __DIR__.'/locale');
 textdomain($local);
 
-if (!defined('SP_AREA')) {
-  #  $app->run();
+if(!defined('SP_AREA')){
+    $app->run();
 }
 
 
-if (false) {
-    echo "<pre>";
-    echo "GET:";
-    print_r($_GET);
 
-    echo "POST:";
-    print_r($_POST);
 
-    echo "SESSION:";
-    print_r($_SESSION);
 
-    echo "</pre>";
-}
-if(false){
-    $endTime = microtime(true);
-    $elapsed = $endTime - $beginn;
-    echo "Execution time : $elapsed seconds";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
